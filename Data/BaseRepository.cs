c00e8252836f93d5eefaf673a803f182e243329d
Thiago Loureiro de Azevedo <thiago.loureiro.de.azevedo@playngo.com>
2018-03-17 21:07:17 +0100

    BaseRepo

---
+++ Data/BaseRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Data
+{
+    public abstract class BaseRepository
+    {
+        public string Connstring = "Data Source=localhost; Initial Catalog=Dapper_Treinamento; Integrated Security=SSPI;";
+    }
+}
